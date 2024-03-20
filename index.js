50f9c5c75e7c4ac9f420c88c23eb0762ff0644d0
cjcackowski <ccackowski@gmail.com>
2024-03-20 00:45:59 -0500

    complete lab

--- index.js
+++ index.js
@@ -4,3 +4,28 @@ const companyName = 'Scuber';
 let mostProfitableNeighborhood = 'Chelsea';
 
 let companyCeo = 'Susan Smith';
+
+function scuberGreetingForFeet(distance) {
+    if (distance <= 400) {
+      return "This one is on me!";
+    } else if (distance > 2000 && distance <= 2500) {
+      return "I will gladly take your thirty bucks.";
+    } else {
+      return "No can do.";
+    }
+  }
+  
+  function ternaryCheckCity(city) {
+    return city === "NYC" ? "Ok, sounds good." : "No go.";
+  }
+  
+  function switchOnCharmFromTip(tip) {
+    switch (tip) {
+      case "generous":
+        return "Thank you so much.";
+      case "not as generous":
+        return "Thank you.";
+      default:
+        return "Bye.";
+    }
+  }
